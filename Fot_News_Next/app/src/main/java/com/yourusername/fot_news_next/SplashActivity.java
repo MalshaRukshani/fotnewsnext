7cb1bbab0e6985ad408867bb876757b8a2a21234
MalshaRukshani <malsharukshani860@gmail.com>
2025-06-12 07:39:20 +0200

    Fot News Next app initial commit

---
+++ Fot_News_Next/app/src/main/java/com/yourusername/fot_news_next/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.yourusername.fot_news_next;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.yourusername.techpulse_newsapp.R;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends BaseActivity{
+
+    private static final int SPLASH_DELAY = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView logo = findViewById(R.id.logoImage);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.startAnimation(fadeIn);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish();
+        }, SPLASH_DELAY);
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.success_color));
+        }
+    }
+}
+
